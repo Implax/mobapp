a3dbbd8bb20f41151ef9dec1f588eda115af2f66
Richard Clottey <richardclotteyjr@gmail.com>
2022-03-31 15:24:41 +0000

    Add files via upload

--- index.php
+++ index.php
@@ -1,3 +1,39 @@
-<?php    $msisdn = $_GET['msisdn'];    $sequence_ID = $_GET['sequenceID'];    $data
-= $_GET['data'];    $type = 1;      $mereply = "Hello Richard Clottey";    echo 
-$mereply.'|'.$msisdn.'|'.$sequence_id.'|'.$type; ?>
+<?php
+    $sessionid = $_POST["sessionId"];
+
+    $serviceCode = $_POST["serviceCode"];
+
+    $phoneNumber = $_POST["phoneNumber"];
+
+    $text = $_POST["text"];
+
+    if ($text = ""){
+        //First Request
+        $response = "CON What would you want to check \n";
+        $response .= "1. Ashesi Student Account \n ";
+        $response .= "2. My Phone Number";
+    } else if (text == "1"){
+        //First Level Response
+        $response = "CON Student information you want to view \n";
+        $response .= "1. Student ID Number";
+        $response .= "2. Student Major";
+
+    } else if ($text =- "2"){
+        $response = "END Your phone number is ".$phoneNumber;
+
+    } else if ($text == "1*1") {
+        $studentID = "12462023"; //HardCoded. Database to be added later
+
+        //A terminal request
+
+        $response = "END Your account Number is".$studentID;
+    } else if ($text = "1*2") {
+        $major = "Computer Science"; //Hardcoded. Database to be added later
+
+        $response = "END Your major is ".$major;
+    }
+
+    header('Content-type' text/plain);
+    echo $response;
+
+?>
